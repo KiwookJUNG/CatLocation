1502f778232dbc9fbf73367f8a5cf2ee4b93ac51
정기욱 <rldnr56@me.com>
2019-03-26 18:52:48 +0900

    커스텀 셀 클래스

---
+++ CatLocation/고양이 테이블뷰/CatCell.swift
@@ -0,0 +1,23 @@
+//
+//  CatCell.swift
+//  CatLocation
+//
+//  Created by 정기욱 on 19/03/2019.
+//  Copyright © 2019 Kiwook. All rights reserved.
+//
+
+import UIKit
+
+class CatCell: UITableViewCell {
+    
+    
+    
+    @IBOutlet var catImg: UIImageView!
+    
+    @IBOutlet var catName: UILabel!
+    
+    @IBOutlet var catDescription: UILabel!
+    
+    @IBOutlet var tnrIs: UILabel!
+    
+}
