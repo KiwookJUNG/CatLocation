18d3c92c2743b5336dc9073683dc7fff241ccc23
정기욱 <rldnr56@me.com>
2019-03-26 18:52:23 +0900

    고양이 데이터 객체

---
+++ CatLocation/고양이 테이블뷰/CatVO.swift
@@ -0,0 +1,24 @@
+//
+//  CatVO.swift
+//  CatLocation
+//
+//  Created by 정기욱 on 19/03/2019.
+//  Copyright © 2019 Kiwook. All rights reserved.
+//
+
+import Foundation
+
+class CatVO {
+    
+    var catImg: String? // 고양이 사진
+    var catName: String? // 고양이 이름
+    var catDescription: String? // 고양이 특징
+    var tnrIs : Bool? // 고양이 tnr여부
+    var territory : String? // 거주지
+    //var latitude : [Double]? // 위도
+    //var longitude : [Double]? // 경도
+    
+    var latitude : Double?
+    var longitude : Double?
+    
+}
